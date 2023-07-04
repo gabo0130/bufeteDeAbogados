3d27e3354c02fddb54f1040595fbe4968c587741
gabo0130 <yoonny0130@hotmail.com>
2023-07-04 18:29:53 -0500

    Cargue inicial de proyecto
    se crea proyecto de spring y se da una carga inicial

---
+++ bufete/src/main/java/com/bufete/bufete/BufeteApplication.java
@@ -0,0 +1,13 @@
+package com.bufete.bufete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BufeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BufeteApplication.class, args);
+	}
+
+}
---
+++ bufete/src/test/java/com/bufete/bufete/BufeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bufete.bufete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BufeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
